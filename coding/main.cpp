738b5a114114ff9fb594a80226e5c2593931d051
Todriew52 <williamkosti@icloud.com>
2025-10-17 00:18:10 +1030

    added test sprite inclusion in window, next include all sprites

--- coding/main.cpp
+++ coding/main.cpp
@@ -9,30 +9,19 @@
 
 int main()
 {
+    //initialise and utilise game engine
     gameEngine game;
-    sf::RenderWindow window(sf::VideoMode(720, 960), "Farming Simulator", sf::Style::Titlebar | sf::Style::Close); //Window creation, 9x12, 16 pixel, 5 scale so tile = 80x80 with each "pixel" being 5 pixels
-    sf::Event pollingEvent; //event created for use of polling
-    window.setFramerateLimit(60); //limit 60 fps
 
-    while (window.isOpen()) //loop for game runs stuff
+    while (game.running()) //loop for game runs stuff
     {
-        //poll events
-        while (window.pollEvent(pollingEvent)) //loop for game again - constant checking for input and stuff
-        {
-            if (pollingEvent.type == sf::Event::Closed || (pollingEvent.type == sf::Event::KeyPressed && pollingEvent.key.code == sf::Keyboard::Escape)) { //for the record, switch cases super recommended here, I just don't like them
-                window.close();
-                return 1; 
-            };
-        };
-
         //update game property stuff
+        game.update();
+
+        //draw new stuff
+        game.draw();
 
         //depiction of update
-        window.clear(sf::Color::Green); //clear old stuff
-            //draw new stuff
-
-            window.display(); //for use after done drawing new stuff so that drawed window is displayed
-    };
-   window.close();
+        game.render();
+            };
     return 0;
 }
